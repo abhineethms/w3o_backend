71318d3e93baa72a4937d66776acae7a32189958
Abhineeth <kineticcode3@gmail.com>
2024-01-26 23:39:32 +0530

    Adding first script file

---
+++ my_script.sh
@@ -0,0 +1,25 @@
+
+Abhineeth@AbhineethPC MINGW64 /
+$ cd desktop
+bash: cd: desktop: No such file or directory
+
+Abhineeth@AbhineethPC MINGW64 /
+$ cd w3o_backend
+bash: cd: w3o_backend: No such file or directory
+
+Abhineeth@AbhineethPC MINGW64 /
+$ cd ~/Desktop
+
+Abhineeth@AbhineethPC MINGW64 ~/Desktop (master)
+$ cd w3o_backend
+
+Abhineeth@AbhineethPC MINGW64 ~/Desktop/w3o_backend (main)
+$ cd init
+bash: cd: init: No such file or directory
+
+Abhineeth@AbhineethPC MINGW64 ~/Desktop/w3o_backend (main)
+$ git init
+Reinitialized existing Git repository in C:/Users/Abhineeth/Desktop/w3o_backend/.git/
+
+Abhineeth@AbhineethPC MINGW64 ~/Desktop/w3o_backend (main)
+$
